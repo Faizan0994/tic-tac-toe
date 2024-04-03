617de870ceaed9a475a3152a5e16120f351df4f7
Faizan <faizanamjad575@gmail.com>
2024-04-03 11:23:58 +0500

    Create function to create gameboard

---
+++ script.js
@@ -0,0 +1,34 @@
+function createGameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+  
+    // Create a 2d array that will represent the state of the game board
+    // For this 2d array, row 0 will represent the top row and
+    // column 0 will represent the left-most column.
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+      }
+    }
+
+    return board;
+}
+
+function Cell() {
+    let value = 0;
+  
+    // Accept a player's token to change the value of the cell
+    const addToken = (player) => {
+      value = player;
+    };
+  
+    // How we will retrieve the current value of this cell through closure
+    const getValue = () => value;
+  
+    return {
+      addToken,
+      getValue
+    };
+}
